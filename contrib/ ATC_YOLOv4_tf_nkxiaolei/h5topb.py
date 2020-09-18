ba5008ae2e53d26113cc4b91564a09c7058df80d
alexcheng88 <0510568@163.com>
2020-09-18 20:42:24 +0800

    add contrib/ ATC_YOLOv4_tf_nkxiaolei/h5topb.py.
    Former-commit-id: 329d998574356420c3336f4142cb4c925cb74153

---
+++ contrib/ ATC_YOLOv4_tf_nkxiaolei/h5topb.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
+def convert_h5to_pb():
+    model = tf.keras.models.load_model("./yolov4.h5",compile=False)
+    model.summary()
+    full_model = tf.function(lambda Input: model(Input))
+    full_model = full_model.get_concrete_function(tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+    # Get frozen ConcreteFunction
+    frozen_func = convert_variables_to_constants_v2(full_model)
+    frozen_func.graph.as_graph_def()
+
+    layers = [op.name for op in frozen_func.graph.get_operations()]
+    print("-" * 50)
+    print("Frozen model layers: ")
+    for layer in layers:
+        print(layer)
+
+    print("-" * 50)
+    print("Frozen model inputs: ")
+    print(frozen_func.inputs)
+    print("Frozen model outputs: ")
+    print(frozen_func.outputs)
+
+    # Save frozen graph from frozen ConcreteFunction to hard drive
+    tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                      logdir="./frozen_models",
+                      name="yolov4.pb",
+                      as_text=False)
+
+
+if __name__ == "__main__":
+    convert_h5to_pb()
