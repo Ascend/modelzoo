3076a4bd018df7d4940570473c572c1ed7bcb212
chengchunlei <alex.cheng@huawei.com>
2020-11-11 09:52:06 +0800

    删除文件 contrib/msame
    Former-commit-id: 69cbe17a080d60bf13d59b792418e97a54e5a10a

--- contrib/msame/build.sh
+++
@@ -1,9 +0,0 @@
-g="$1"
-locate="$2"
-rm -rf build
-mkdir -p build/intermediates/host
-cd build/intermediates/host
-cmake ../../../src -DCMAKE_CXX_COMPILER=${g} -DCMAKE_SKIP_RPATH=TRUE
-make
-cd ../../../out
-mv main ${locate}/msame
