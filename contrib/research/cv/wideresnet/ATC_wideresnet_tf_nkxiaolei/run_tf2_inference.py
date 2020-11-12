ab65b09956094350f746febfb2eb894eab2c1046
chengchunlei <alex.cheng@huawei.com>
2020-11-12 19:21:41 +0800

    删除文件 contrib/research/cv/wideresnet/ATC_wideresnet_tf_nkxiaolei/run_tf2_inference.py
    Former-commit-id: 82b9ec39ff98487f1118f92ce37f3b0f3cf63e04

--- contrib/research/cv/wideresnet/ATC_wideresnet_tf_nkxiaolei/run_tf2_inference.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
-import pdb
-import numpy as np
-
-model = tf.keras.models.load_model('./Wide_ResNet/')
-
-imgs = np.fromfile("./input/000000.bin",dtype="float32").reshape(1,32,32,3)
-print(model.predict(imgs))
