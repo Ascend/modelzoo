1acaae33fe598f30b991a1a08cc3d95058ccff6f
chengchunlei <alex.cheng@huawei.com>
2020-11-20 17:16:18 +0800

    更新自测试用例
    Former-commit-id: ba384a0f87a5ae14df80c037830e37a18f28cb73

--- contrib/Research/cv/efficientnet-b8/ATC_efficientnet-b8_tf_nkxiaolei/offline_inference_testcase.sh
+++ contrib/Research/cv/efficientnet-b8/ATC_efficientnet-b8_tf_nkxiaolei/offline_inference_testcase.sh
@@ -11,10 +11,5 @@ mkdir $input
 python3.7.5 img_preprocess.py --src_path=$originnal_pic --dst_path=$input --pic_num=100
 cp $ori_model $model
 rm -rf $output/*
-#start infence
-./msame --model $model --input $input --output $output
-
-#top1 accuarcy
-python3.7.5 accuarcy_top1.py $output $label
-
-echo "Run testcase success!"
+#start inference and postprocess
+python3 testcase.py $model $input $output $label
