abf6005ae5de8296c92314289c696dbc283a2ae2
chengchunlei <alex.cheng@huawei.com>
2020-11-21 15:57:43 +0800

    update contrib/Research/cv/efficientnet-b8
    Former-commit-id: 61bb6103233da36e46092a7b092b6cb46ec9bed3

--- contrib/Research/cv/efficientnet-b8/ATC_efficientnet-b8_tf_nkxiaolei/offline_inference_testcase.sh
+++ contrib/Research/cv/efficientnet-b8/ATC_efficientnet-b8_tf_nkxiaolei/offline_inference_testcase.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 #Ascend社区已预置的数据集、预训练模型、ATC-OM模型等
-originnal_pic="/data/ModelZoo/Dataset/ILSVRC2012_val/"
-ori_model="/data/ModelZoo/Models/nkxiaolei/EfficientNet-B8/efficientnet-b8.om"
+originnal_pic=$ILSVRC2012_VAL
+model="$OBS_BUCKET/nkxiaolei/EfficientNet-B8/efficientnet-b8.om"
 input="input/"
 output="output/"
 label="ground_truth/val_map.txt"
-model=ori_model
-#开发个人独立预置的数据集、预训练模型、ATC-OM模型等，支持从OBS仓下载
 
-#case主体，开发者根据不同模型写作
+#开发个人独立预置的数据集、预训练模型、ATC-OM模型等，支持从OBS仓下载
+#obsutil cp obs://obsxxx/xxx/xxx.om ./model/ -f -r
+
+#testcase主体，开发者根据不同模型写作
 python3.7.5 img_preprocess.py --src_path=$originnal_pic --dst_path=$input --pic_num=100
 
 #start inference and postprocess
