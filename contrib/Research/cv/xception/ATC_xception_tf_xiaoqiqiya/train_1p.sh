dabff93b25236356de8d0979c2d16231f0fd7908
xiaoqiqiya <8376014+xiaoqiqiyaya@user.noreply.gitee.com>
2020-12-06 16:45:02 +0800

    删除文件 contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/train_1p.sh

--- contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/train_1p.sh
+++
@@ -1,6 +0,0 @@
-
-#train
-python    run_xception.py  --model_path ./model/xception_model.ckpt  --data_path ./data/train_1.tfrecord  --output_path  ./model_save  --do_train True  --image_num  50000 --class_num  1000  --batch_size  64  --epoch  10 --learning_rate  0.001   --save_checkpoints_steps  100
-
-
-          
