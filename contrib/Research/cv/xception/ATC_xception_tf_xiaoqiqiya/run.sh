cfb326d5585d27c29a305e33c7f683968bd1b867
xiaoqiqiya <8376014+xiaoqiqiyaya@user.noreply.gitee.com>
2020-12-06 16:49:07 +0800

    update contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/run.sh.

--- contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/run.sh
+++ contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/run.sh
@@ -1,4 +1,4 @@
 
-python    run_xception.py  --model_path None   --data_path /sda2/imagenettfrecord/tfrecord_299/train_tf_299  --output_path  ./model_save3  --do_train True  --image_num  50000 --class_num  1000  --batch_size  64  --epoch  10 --learning_rate  0.001   --save_checkpoints_steps  10
+python    run_xception.py  --model_path ./model/xception_model.ckpt   --data_path ./train_data  --output_path  ./model_save  --do_train True  --image_num  1281167 --class_num  1000  --batch_size  64  --epoch  10 --learning_rate  0.001   --save_checkpoints_steps  1000
 
           
