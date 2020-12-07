7bae9fac84ddb4888980dc57795144d9809a7622
xiaoqiqiya <8376014+xiaoqiqiyaya@user.noreply.gitee.com>
2020-12-07 11:46:31 +0800

    update contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/train_1p.sh.

--- contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/train_1p.sh
+++ contrib/Research/cv/xception/ATC_xception_tf_xiaoqiqiya/train_1p.sh
@@ -1,4 +1,4 @@
 
-python    run_xception.py  --model_path ./model/xception_model.ckpt   --data_path ./train_data  --output_path  ./model_save  --do_train True  --image_num  1281167 --class_num  1000  --batch_size  64  --epoch  10 --learning_rate  0.001   --save_checkpoints_steps  1000
+python    run_xception.py  --model_path ./model/xception_model.ckpt   --data_path ./train_data  --output_path  ./model_save  --do_train True  --image_num  1281167 --class_num  1000  --batch_size  64  --epoch  10 --learning_rate  0.01   --save_checkpoints_steps  1000
 
           
