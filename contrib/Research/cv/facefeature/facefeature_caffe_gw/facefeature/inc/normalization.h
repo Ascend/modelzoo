6d1a25e4c8927facad79f6f500b7d2f9f5ae67e9
guo  wen <guowen16@huawei.com>
2020-12-10 15:09:45 +0800

    add

--- contrib/Research/cv/facefeature/facefeature_caffe_gw/facefeature/inc/normalization.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cmath>
-#include <cfloat>
-#include "utils.h"
-
-float L2NormSum(const float* vectorBuf, int size);
-void L2Normalization(float* vectorBuf, int size);
-float CalSimilarity(const float* x, const float* y, const int size);
-int SearchFeatureLib(std::vector<FaceInfo> featureLib, float* featureVector, int featureSize, float& similarity);
