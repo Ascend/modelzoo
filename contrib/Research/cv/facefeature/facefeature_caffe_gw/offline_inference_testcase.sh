bf93bbd463531f7010473bf944949faea8376bcd
guo  wen <guowen16@huawei.com>
2020-12-10 17:34:45 +0800

    up

--- contrib/Research/cv/facefeature/facefeature_caffe_gw/offline_inference_testcase.sh
+++ contrib/Research/cv/facefeature/facefeature_caffe_gw/offline_inference_testcase.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 #start inference
 cd facefeature
-./offline_inference_testcase.sh
+./out/main ./data/ 2>&1 > inference.log
+
+expect_time=20
+#性能检查耗时
+avg_time=`grep "Inference average time without first time:" inference.log | awk '{print $8}'`
+echo "Average inference time is $avg_time ms, expect time is <$expect_time ms"
+
+if [ $avg_time -lt $expect_time ];then
+   echo "Run testcase success!"
+else
+   echo "Run testcase failed!"
+fi
+
