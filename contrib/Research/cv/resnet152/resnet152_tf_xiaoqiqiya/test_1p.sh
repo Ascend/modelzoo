54445f85f059f4032835c43968f09735ffd54de1
xiaoqiqiya <8376014+xiaoqiqiyaya@user.noreply.gitee.com>
2020-12-10 19:46:32 +0800

    update contrib/Research/cv/resnet152/resnet152_tf_xiaoqiqiya/test_1p.sh.

--- contrib/Research/cv/resnet152/resnet152_tf_xiaoqiqiya/test_1p.sh
+++ contrib/Research/cv/resnet152/resnet152_tf_xiaoqiqiya/test_1p.sh
@@ -1,2 +1,2 @@
-python    run_resnet.py  --model_path ./model/resnet.ckpt --data_path ./test_data/    --image_num  50000 --class_num  1000  --batch_size  100
+python    run_resnet.py  --model_path ./model/resnet_model.ckpt --data_path ./test_data/    --image_num  50000 --class_num  1000  --batch_size  100
           
