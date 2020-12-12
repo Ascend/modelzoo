5111e9b7d8612fdb4b69a29b49e2ad7fb1fd24f6
wujianping <wujianping.2006@163.com>
2020-12-12 20:24:57 +0800

    mobilenetV2_pytorch model scripts

---
+++ built-in/PyTorch/Official/cv/image_classification/MobileNetV2_for_PyTorch/1p/run1p.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+source npu_set_env.sh
+su HwHiAiUser -c "/usr/local/Ascend/ascend-toolkit/latest/toolkit/bin/adc --host 172.17.0.1:22118 --log \"SetLogLevel(0)[error]\" --device 0"
+
+currentDir=$(cd "$(dirname "$0")";pwd)
+currtime=`date +%Y%m%d%H%M%S`
+train_log_dir=${currentDir}/result/training_1p_job_${currtime}
+mkdir -p ${train_log_dir}
+cd ${train_log_dir}
+echo "train log path is ${train_log_dir}"
+
+export SLOG_PRINT_TO_STDOUT=0
+export TASK_QUEUE_ENABLE=1
+python3.7 ${currentDir}/main_apex.py \
+    --seed 49  \
+    --workers 128 \
+    --lr 0.05 \
+    --amp \
+    --opt-level 'O2' \
+    --loss-scale-value 64 \
+    --momentum 0.9 \
+    --batch-size 512 \
+    --weight-decay 1e-5 \
+    --epochs 600 \
+    --print-freq 1 \
+    --eval-freq 1 \
+    --device 'npu:0' \
+    --summary-path './runs/mobilenetv2/log' \
+    --data /data/imagenet > ${train_log_dir}/train_1p.log 2>&1 &
---
+++ built-in/PyTorch/Official/cv/image_classification/MobileNetV2_for_PyTorch/8p/run8p.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+source npu_set_env.sh
+
+su HwHiAiUser -c "/usr/local/Ascend/ascend-toolkit/latest/toolkit/bin/adc --host 172.17.0.1:22118 --log \"SetLogLevel(0)[error]\" --device 0"
+su HwHiAiUser -c "/usr/local/Ascend/ascend-toolkit/latest/toolkit/bin/adc --host 172.17.0.1:22118 --log \"SetLogLevel(0)[error]\" --device 4"
+
+currentDir=$(cd "$(dirname "$0")";pwd)
+currtime=`date +%Y%m%d%H%M%S`
+train_log_dir=${currentDir}/result/training_8p_job_${currtime}
+mkdir -p ${train_log_dir}
+cd ${train_log_dir}
+echo "train log path is ${train_log_dir}"
+
+export SLOG_PRINT_TO_STDOUT=0
+export TASK_QUEUE_ENABLE=1
+nohup python3.7 ${currentDir}/mobilenetv2_8p_main_anycard.py \
+    --addr=$(hostname -I |awk '{print $1}') \
+    --seed 49  \
+    --workers 128 \
+    --lr 0.4 \
+    --print-freq 1 \
+    --eval-freq 1 \
+    --dist-url 'tcp://127.0.0.1:50002' \
+    --dist-backend 'hccl' \
+    --multiprocessing-distributed \
+    --world-size 1 \
+    --batch-size 4096 \
+    --epochs 600 \
+    --rank 0 \
+    --device-list '0,1,2,3,4,5,6,7' \
+    --amp \
+    --benchmark 0 \
+    --data /home/imagenet > ${train_log_dir}/train_8p.log 2>&1 &
+
