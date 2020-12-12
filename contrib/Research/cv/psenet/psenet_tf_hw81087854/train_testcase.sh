169619b17b5524629c31eec39a34641e2c2dea24
king123 <598069874@qq.com>
2020-12-12 21:18:37 +0800

    update contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh.

--- contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh
+++ contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh
@@ -1,13 +1,12 @@
 #Need dataset of icdar2015
 
-:<<!
-python npu_train.py \
+python3.7 npu_train.py \
+--max_steps=20  \
 --input_size=512 \
 --batch_size_per_gpu=8 \
 --checkpoint_path=./resnet_v1_50/ \
---training_data_path=./ocr/icdar2015/ >train.log 2>&1
-!
-echo "Run testcase success!"
+--training_data_path=$icdar2015_train >train.log 2>&1
+
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
 key1="[GEOP]"  #功能检查字
 key2="xxx"  #性能检查字
