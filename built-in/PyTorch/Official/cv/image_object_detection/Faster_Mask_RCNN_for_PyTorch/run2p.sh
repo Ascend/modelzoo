25614d1c3ee26729f95c8f38d72e5ad7412f2fc8
root <root@DESKTOP-9IT9GMJ.localdomain>
2020-12-16 10:32:20 +0800

    add run2p.sh

---
+++ built-in/PyTorch/Official/cv/image_object_detection/Faster_Mask_RCNN_for_PyTorch/run2p.sh
@@ -0,0 +1,23 @@
+source ./env_b031.sh
+export PYTHONPATH=./:$PYTHONPATH
+export SLOG_PRINT_TO_STDOUT=0
+su HwHiAiUser -c "adc --host 0.0.0.0:22118 --log \"SetLogLevel(0)[error]\" --device 0"
+su HwHiAiUser -c "adc --host 0.0.0.0:22118 --log \"SetLogLevel(0)[error]\" --device 4"
+su HwHiAiUser -c "adc --host 0.0.0.0:22118 --log \"SetLogLevel(2)[disable]\" "
+
+nohup python3.7 tools/train_net.py \
+        --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml \
+        --num-gpus 2\
+        --debug-mode 0\
+        --device-ids 6 7\
+        AMP 1\
+        OPT_LEVEL O2 \
+        LOSS_SCALE_VALUE 64 \
+        SOLVER.IMS_PER_BATCH 4 \
+        SOLVER.MAX_ITER 77000 \
+        SEED 1234 \
+        MODEL.RPN.NMS_THRESH 0.8 \
+        MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO 2 \
+        MODEL.ROI_MASK_HEAD.POOLER_SAMPLING_RATIO 2 \
+        DATALOADER.NUM_WORKERS 4 \
+        SOLVER.BASE_LR 0.005 &
