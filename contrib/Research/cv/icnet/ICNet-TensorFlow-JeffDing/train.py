51b306cba403f7bb30d0294a42b490d16bc83031
JeffDing <JeffDing890430@163.com>
2020-12-18 08:04:07 +0800

    fix

--- contrib/Research/cv/icnet/ICNet-TensorFlow-JeffDing/train.py
+++ contrib/Research/cv/icnet/ICNet-TensorFlow-JeffDing/train.py
@@ -40,10 +40,11 @@ from utils.visualize import decode_labels
 from utils.image_reader import ImageReader, prepare_label
 
 from npu_bridge.estimator import npu_ops 
-from tensorflow.core.protobuf.rewriter_config_pb2 import RewriterConfig 
+from tensorflow.core.protobuf.rewriter_config_pb2 import RewriterConfig
+
 i_list = [2025, 7921, 32041]
 i_count = 0
-from tensorflow import float32  
+from tensorflow import float32
 
 
 def get_arguments():
@@ -63,10 +64,6 @@ def get_arguments():
     parser.add_argument("--filter-scale", type=int, default=1,
                         help="1 for using pruned model, while 2 for using non-pruned model.",
                         choices=[1, 2])
-    #parser.add_argument("--dataurl", required=True,
-    #                    help="Which dataset to trained with Ascend 910",
-    #                    choices=['$cityscapes_dataset', 'others'])
-    
     return parser.parse_args()
 
 def get_mask(gt, num_classes, ignore_label):
@@ -78,7 +75,6 @@ def get_mask(gt, num_classes, ignore_label):
     #indices = tf.ones([32041,], dtype=tf.int32)
 
     '''
-
     less_equal_class = tf.less_equal(gt, 18)
     not_equal_ignore = tf.not_equal(gt, 255)
     mask = tf.logical_and(less_equal_class, not_equal_ignore)
@@ -91,39 +87,25 @@ def get_mask(gt, num_classes, ignore_label):
             else:
                 mask[i][j] = 0
     indices = mask.astype(np.int32)     
-    
-
     return indices
-    
-    
-
-    
-def create_loss(output, label, num_classes, ignore_label):
-    # raw_pred = tf.reshape(output, [-1, num_classes]) # 原始代码
-    
-    tmp_tianyili = tf.size(output) // 19
-    raw_pred = tf.reshape(output, [tmp_tianyili, num_classes]) 
-
-    
-
+        
+def create_loss(output, label, num_classes, ignore_label):   
+    tmp_loss = tf.size(output) // 19
+    raw_pred = tf.reshape(output, [tmp_loss, num_classes]) 
     label = prepare_label(label, tf.stack(output.get_shape()[1:3]), num_classes=num_classes, one_hot=False)
     
     global i_list
     global i_count
-    # print("i_list[i_count]", i_list[i_count])
     label = tf.reshape(label, [i_list[i_count],])
     i_count = i_count + 1
     if i_count > 2:
         i_count = 0
-    
-
     indices = get_mask(label, num_classes, ignore_label)
     gt = tf.cast(tf.gather(label, indices), tf.int32)
     pred = tf.gather(raw_pred, indices)
 
     loss = tf.nn.softmax(logits=pred)
     reduced_loss = tf.reduce_mean(loss) 
-    #reduced_loss = loss
 
     return reduced_loss
 
@@ -152,7 +134,6 @@ class TrainConfig(Config):
     # Note that you need to use "bnnomerge" version.
     ######################################################################################################################3
     model_weight = './model/cityscapes/icnet_cityscapes_train_30k_bnnomerge.npy'
-    #model_weight = ''
     
     # Set hyperparameters here, you can get much more setting in Config Class, see 'utils/config.py' for details.
     LAMBDA1 = 0.16
@@ -164,7 +145,6 @@ class TrainConfig(Config):
 def main():
     """Create the model and start the training."""
     args = get_arguments()
-    #print("args --dataurl:", args.dataurl)
 
     """
     Get configurations here. We pass some arguments from command line to init configurations, for training hyperparameters, 
@@ -215,13 +195,7 @@ def main():
         opt_conv = tf.train.MomentumOptimizer(learning_rate, cfg.MOMENTUM)
        
         grads = tf.gradients(reduced_loss, all_trainable)
-        #print("测试测试测试：grads：", grads)
-        #grads = tf.ones([3, 3, 3, 32], dtype=float32)
-        #print("测试测试grads_constant", grads_constant)
-        #grads = tf.constant(grads_constant, dtype=tf.float32,shape=(3, 3, 3, 32))
-        #print("测试测试测试grads_constant", grads_constant)
         train_op = opt_conv.apply_gradients(zip(grads, all_trainable))
-        # print("测试测试测试：train_op：", train_op)
     
     # Create session & restore weights (Here we only need to use train_net to create session since we reuse it)
     train_net.create_session()
@@ -240,7 +214,6 @@ def main():
             custom_op.name =  "NpuOptimizer"
             custom_op.parameter_map["use_off_line"].b = True # 必须显示开启，在昇腾AI处理器执行训练
             config.graph_options.rewrite_options.remapping = RewriterConfig.OFF  # 必须显示关闭remap
-            # config.graph_options.rewrite_options.optimizers.extend(["GradFusionOptimizer"]) #分布式添加
 
             loss_value, loss1, loss2, loss3, val_loss_value = train_net.sess.run([reduced_loss, loss_sub4, loss_sub24, loss_sub124, val_reduced_loss], feed_dict=feed_dict)
             
