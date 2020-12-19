052bfd66755fa77b0c40561701278a726cc8be06
JeffDing <JeffDing890430@163.com>
2020-12-19 10:16:32 +0800

    fix script

--- contrib/Research/cv/east/east-TensorFlow-JeffDing/train_testcase.sh
+++ contrib/Research/cv/east/east-TensorFlow-JeffDing/train_testcase.sh
@@ -15,7 +15,7 @@ python npu_train.py \
 	--geometry=RBOX \
 	--learning_rate=0.0001 \
 	--num_readers=24 \
-	--max_steps=200 \
+	--max_steps=20 \
 	--save_checkpoint_steps=10 >train.log 2>&1
     #--model_dir=./model_path
 
