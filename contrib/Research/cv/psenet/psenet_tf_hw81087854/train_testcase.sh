49d88a7ef79ade9e7ef0abb52ff00fdefa7481d3
king123 <king12387@163.com>
2020-12-19 21:13:52 +0800

    update contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh.

--- contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh
+++ contrib/Research/cv/psenet/psenet_tf_hw81087854/train_testcase.sh
@@ -9,10 +9,10 @@ python3.7 npu_train.py \
 
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
 key1="[GEOP]"  #功能检查字
-key2="xxx"  #性能检查字
+key2="total loss"  #性能检查字
 key3="xxx"  #精度检查字
 
-if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
+if [ `grep -c "$key1" "train.log"` -ne '0' ] && [ `grep -c "$key2" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
    echo "Run testcase success!"
 else
    echo "Run testcase failed!"
