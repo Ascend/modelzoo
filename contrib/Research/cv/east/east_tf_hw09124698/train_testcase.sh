cce835f9a231db558fbeba1d1e378a39cb3f4ea5
aoe <hehejd@qq.com>
2020-12-21 15:42:52 +0800

    update contrib/Research/cv/east/east_tf_hw09124698/train_testcase.sh.

--- contrib/Research/cv/east/east_tf_hw09124698/train_testcase.sh
+++ contrib/Research/cv/east/east_tf_hw09124698/train_testcase.sh
@@ -25,7 +25,7 @@ python multigpu_train.py \
 
 end_time=`date +%s`
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 
 
 if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
@@ -33,7 +33,11 @@ if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整�
 else
    echo "Run testcase failed!"
 fi
-
+if [ `grep -c "Step" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
+   echo "Run testcase success!"
+else
+   echo "Run testcase failed!"
+fi
 cat train.log |grep Step
 
 echo execution time was `expr $end_time - $start_time` s.
