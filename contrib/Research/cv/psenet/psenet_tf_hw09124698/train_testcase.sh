b8b2a954170a193c103adfafd0169621f2852c13
aoe <hehejd@qq.com>
2020-12-21 15:42:15 +0800

    update contrib/Research/cv/psenet/psenet_tf_hw09124698/train_testcase.sh.

--- contrib/Research/cv/psenet/psenet_tf_hw09124698/train_testcase.sh
+++ contrib/Research/cv/psenet/psenet_tf_hw09124698/train_testcase.sh
@@ -18,7 +18,7 @@ python train.py \
 
 end_time=`date +%s`
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 
 
 if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
@@ -26,5 +26,9 @@ if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整�
 else
    echo "Run testcase failed!"
 fi
-
+if [ `grep -c "Step" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
+   echo "Run testcase success!"
+else
+   echo "Run testcase failed!"
+fi
 cat train.log |grep Step
