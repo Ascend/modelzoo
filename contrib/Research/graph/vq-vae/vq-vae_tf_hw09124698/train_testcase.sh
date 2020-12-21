fae44f7fa09938f98bddd57035681d7beaffc722
aoe <hehejd@qq.com>
2020-12-21 15:43:18 +0800

    update contrib/Research/graph/vq-vae/vq-vae_tf_hw09124698/train_testcase.sh.

--- contrib/Research/graph/vq-vae/vq-vae_tf_hw09124698/train_testcase.sh
+++ contrib/Research/graph/vq-vae/vq-vae_tf_hw09124698/train_testcase.sh
@@ -9,7 +9,7 @@ python train.py > train.log 2>&1
 python extract_z.py >> train.log 2>&1
 python train_prior.py >> train.log 2>&1
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 
 
 if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
