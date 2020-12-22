0d023ed86a04fa3535259e647322561769f34461
skywalk <skywalk.duan@gmail.com>
2020-12-22 18:51:37 +0800

    线上贡献

--- contrib/Research/cv/east/EAST_tf_skywalk163/train_testcase.sh
+++ contrib/Research/cv/east/EAST_tf_skywalk163/train_testcase.sh
@@ -5,8 +5,9 @@ execpath=${PWD}
 # rm -rf *.pbtxt
 ulimit -c 0
 
-#python npu_train.py $CIFAR10 > train.log 2>&1
-python npu_train.py  > train.log 2>&1
+python npu_train.py --training_data_path=$icdar2015_train > train.log 2>&1
+# python npu_train.py  > train.log 2>&1
+
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
 key1="/[GEOP/]"  #功能检查字
 key2="loss"
