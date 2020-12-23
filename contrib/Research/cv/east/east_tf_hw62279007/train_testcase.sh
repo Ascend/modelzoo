8534dd4e5791dca8155e8ec8154acc49195d70c7
cooem <mesiasfskvkgt@gmail.com>
2020-12-23 22:15:39 +0800

    update contrib/Research/cv/east/east_tf_hw62279007/train_testcase.sh.

--- contrib/Research/cv/east/east_tf_hw62279007/train_testcase.sh
+++ contrib/Research/cv/east/east_tf_hw62279007/train_testcase.sh
@@ -8,7 +8,7 @@ python multigpu_train.py \
 		--max_steps=20 \
 		--save_checkpoint_steps=10 >train.log 2>&1
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 key1="total loss"  #功能检查字
 
 if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
