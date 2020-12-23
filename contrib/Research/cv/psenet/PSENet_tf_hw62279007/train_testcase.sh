4f8173f56634ffabf61c6e67769154c50504ba0a
cooem <mesiasfskvkgt@gmail.com>
2020-12-23 22:15:59 +0800

    update contrib/Research/cv/psenet/PSENet_tf_hw62279007/train_testcase.sh.

--- contrib/Research/cv/psenet/PSENet_tf_hw62279007/train_testcase.sh
+++ contrib/Research/cv/psenet/PSENet_tf_hw62279007/train_testcase.sh
@@ -3,7 +3,7 @@ python train.py \
 		--max_steps=20 >train.log 2>&1
 		
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 key1="total loss"  #功能检查字
 
 if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
