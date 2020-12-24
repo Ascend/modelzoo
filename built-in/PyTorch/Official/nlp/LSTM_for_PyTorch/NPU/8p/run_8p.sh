57262151c70b44f8010fdbfa6211823772fdc955
luo <luoweidong8@huawei.com>
2020-12-24 16:47:00 +0800

    适配最新海思包取消adc日志适配

--- built-in/PyTorch/Official/nlp/LSTM_for_PyTorch/NPU/8p/run_8p.sh
+++ built-in/PyTorch/Official/nlp/LSTM_for_PyTorch/NPU/8p/run_8p.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-su HwHiAiUser -c "/usr/local/Ascend/ascend-toolkit/latest/toolkit/bin/adc --host 0.0.0.0:22118 --log \"SetLogLevel(0)[error]\" --device 0"
-su HwHiAiUser -c "/usr/local/Ascend/ascend-toolkit/latest/toolkit/bin/adc --host 0.0.0.0:22118 --log \"SetLogLevel(0)[error]\" --device 4"
-
 #Author: Ruchao Fan
 #2017.11.1     Training acoustic model and decode with phoneme-level bigram
 #2018.4.30     Replace the h5py with ark and simplify the data_loader.py
