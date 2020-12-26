7959166861549cae33a93b965a3337181f989954
xiaoqiqiya <8376014+xiaoqiqiyaya@user.noreply.gitee.com>
2020-12-26 13:38:06 +0800

    update contrib/Research/cv/wideresnet/wideresnet_tf_xiaoqiqi/train_1p.sh.

--- contrib/Research/cv/wideresnet/wideresnet_tf_xiaoqiqi/train_1p.sh
+++ contrib/Research/cv/wideresnet/wideresnet_tf_xiaoqiqi/train_1p.sh
@@ -1,4 +1,4 @@
 
-python    run_wideresnet.py  --model_path ./model/wideresnet.ckpt   --data_path ./train_data --output_path  ./model_save   --depths 28  --ks 10 --do_train True  --image_num  50000   --batch_size  32 --epoch  10 --learning_rate  0.00016
+python    run_wideresnet.py  --model_path ./model/wideresnet.ckpt   --data_path ./train_data --output_path  ./model_save   --depths 28  --ks 10 --do_train True  --image_num  50000   --batch_size  32 --epoch  30 --learning_rate  0.00016
 
           
