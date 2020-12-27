63f51cc1cdb35346b2794f36494b54f1e4ac9e8a
黑曜 <regressionline@163.com>
2020-12-27 12:33:10 +0800

    update contrib/Research/cv/svgd/svgd_tf_hw46314258/1_gaussian_mixture.py.

--- contrib/Research/cv/svgd/svgd_tf_hw46314258/1_gaussian_mixture.py
+++ contrib/Research/cv/svgd/svgd_tf_hw46314258/1_gaussian_mixture.py
@@ -46,7 +46,7 @@ tf.set_random_seed(seed)
 
 
 def network(scope):
-
+    '''network'''
     with tf.variable_scope(scope):
         x = tf.Variable(initial_xs[eval(scope[1:])])
         log_prob0, log_prob1 = tf_log_normal(x, -2., 1.), tf_log_normal(x, 2., 1.)
@@ -58,6 +58,7 @@ def network(scope):
 
 
 def make_gradient_optimizer():
+    '''make gradient optimizer'''
     return AdagradOptimizer(learning_rate=learning_rate)
 
 
@@ -96,6 +97,7 @@ with tf.Session() as sess:
 
     # plot
     def plot():
+        '''plot'''
         fig = plt.figure(figsize=(5, 5))
         ax = fig.add_subplot(111)
         x_grid = np.linspace(-15, 15, 200)
@@ -109,6 +111,7 @@ with tf.Session() as sess:
         ax.scatter(final_xs, np.zeros_like(final_xs), color='red')
 
         def log_normal(x, m, s):
+            '''log normal'''
             return - (x - m) ** 2 / 2. / s ** 2 - np.log(s) - 0.5 * np.log(2. * np.pi)
         target_density = np.exp(log_normal(x_grid, -2., 1.)) / 3 + np.exp(log_normal(x_grid, 2., 1.)) * 2 / 3
         ax.plot(x_grid, target_density, 'r--', label='target density')
