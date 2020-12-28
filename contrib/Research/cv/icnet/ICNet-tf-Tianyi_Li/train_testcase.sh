e48c08aed5d48b7b1205c44c7c2c02b2f55e5892
TianyiLi <1375088614@qq.com>
2020-12-28 20:45:55 +0800

    update contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh.

--- contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh
+++ contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh
@@ -21,7 +21,7 @@ python3.7 train.py \
   #--model_dir=./model_path
 end_time=`date +%s`
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
-key1="[GEOP]"  #功能检查字
+key1="\[GEOP\]"  #功能检查字
 key2="total loss"  #性能检查字
 key3="val_loss" #精度检查字
 
