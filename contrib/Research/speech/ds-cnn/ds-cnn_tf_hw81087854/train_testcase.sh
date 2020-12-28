6a2597b11872a40ab47ac2437c2b65f5bb3ba4f2
king123 <king12387@163.com>
2020-12-28 22:18:54 +0800

    update contrib/Research/speech/ds-cnn/ds-cnn_tf_hw81087854/train_testcase.sh.

--- contrib/Research/speech/ds-cnn/ds-cnn_tf_hw81087854/train_testcase.sh
+++ contrib/Research/speech/ds-cnn/ds-cnn_tf_hw81087854/train_testcase.sh
@@ -1,5 +1,5 @@
 export EXPERIMENTAL_DYNAMIC_PARTITION=1
-python3.7 train.py --model_architecture dnn --model_size_info 128 128 128
+python3.7 train.py --model_architecture dnn --model_size_info 128 128 128 >train.log 2>&1
 
 #结果判断，功能检查输出ckpt/日志关键字、精度检查loss值/accucy关键字、性能检查耗时打点/ThroughOutput等关键字
 key1="[GEOP]"  #功能检查字
