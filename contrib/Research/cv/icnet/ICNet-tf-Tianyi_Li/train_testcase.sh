0b440365072caf8f9e891e5c8227e3f8c81c5368
TianyiLi <1375088614@qq.com>
2020-12-28 20:28:21 +0800

    update contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh.

--- contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh
+++ contrib/Research/cv/icnet/ICNet-tf-Tianyi_Li/train_testcase.sh
@@ -3,18 +3,18 @@ export POD_NAME=another0
 
 execpath=${PWD}
 
-#output="./output_base_v2"
-# rm -rf *.pbtxt
 ulimit -c 0
 
 start_time=`date +%s`
 pip install opencv-python jupyter matplotlib tqdm
 python -m pip install cityscapesscripts
-python train.py \
-  --update-mean-var=False \
-  --train-beta-gamma=False \
-  --random-scale=False \
-  --random-mirror=False \
+export EXPERIMENTAL_DYNAMIC_PARTITION=1
+#python train.py --update-mean-var --train-beta-gamma --random-scale --random-mirror --dataset cityscapes --filter-scale 1 --dataurl others
+python3.7 train.py \
+  --update-mean-var \
+  --train-beta-gamma \
+  --random-scale \
+  --random-mirror \
   --dataset=cityscapes \
   --dataurl=others \
   --filter-scale=1 >train.log 2>&1
@@ -25,13 +25,9 @@ key1="[GEOP]"  #功能检查字
 key2="total loss"  #性能检查字
 key3="val_loss" #精度检查字
 
-if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
-   echo " Run testcase success!"   
-fi
-if [ `grep -c "$key2" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
-   echo " Run testcase success!"
-fi
-if [ `grep -c "$key3" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
-   echo " Run testcase success!"
+if [ `grep -c "$key1" "train.log"` -ne '0' ] && [ `grep -c "$key2" "train.log"` -ne '0' ] && [ `grep -c "$key3" "train.log"` -ne '0' ];then 
+   echo "Get [GEOP] and total loss, Run testcase success!"   
+else
+   echo "Run testcase failed!"
 fi
 echo execution time was `expr $end_time - $start_time` s.
