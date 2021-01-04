d61efe0372650fa51c50070ebe13688150338bcf
lzyhha <819814373@qq.com>
2021-01-04 20:45:40 +0800

    sys

--- contrib/Research/cv/pwcnet/PWCNet_tf_wosaisai/offline_infer/dataprep.py
+++ contrib/Research/cv/pwcnet/PWCNet_tf_wosaisai/offline_infer/dataprep.py
@@ -25,7 +25,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import sys
+sys.path.append('..')
 
 from dataset_base import _DEFAULT_DS_VAL_OPTIONS
 from dataset_mpisintel import MPISintelDataset
