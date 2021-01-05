96e1e46a5c6730e1156dc8cf914ef93bdcd9d0fd
cooem <mesiasfskvkgt@gmail.com>
2021-01-05 14:55:32 +0800

    update contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/train_testcase.sh.

--- contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/train_testcase.sh
+++ contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/train_testcase.sh
@@ -3,15 +3,8 @@ python train.py --data_dir ./outdata/ --seq_length 3 --reconstr_weight 0.85 --sm
 key1="\[GEOP\]"  #功能检查字
 
 
-if [ `grep -c "$key1" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
+if [ `grep -c "$key1" "train.log"` -ne '0' ] && [ `grep -c "Epoch" "train.log"` -ne '0' ];then   #可以根据需要调整检查逻辑
    echo "Run testcase success!"
 else
    echo "Run testcase failed!"
 fi
-
-if [ `grep -c "Epoch" "train.log"` -ne '0' ] ;then   #可以根据需要调整检查逻辑
-   echo "Run testcase success!"
-else
-   echo "Run testcase failed!"
-fi
-
