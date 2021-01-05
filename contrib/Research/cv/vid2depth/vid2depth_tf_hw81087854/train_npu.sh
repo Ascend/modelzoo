fbdc73c217e4e495c588ecfd267deb66882c1b2c
king12387 <king12387@163.com>
2021-01-05 23:41:10 +0800

    Add Vid2Depth Network

---
+++ contrib/Research/cv/vid2depth/vid2depth_tf_hw81087854/train_npu.sh
@@ -0,0 +1,10 @@
+python3.7 train.py \
+  --data_dir ./data/kitti_raw_eigen \
+  --seq_length 3 \
+  --reconstr_weight 0.85 \
+  --smooth_weight 0.05 \
+  --ssim_weight 0.15 \
+  --icp_weight 0 \
+  --train_steps 1 \
+  --summary_freq 1 \
+  --checkpoint_dir ./checkpoints
