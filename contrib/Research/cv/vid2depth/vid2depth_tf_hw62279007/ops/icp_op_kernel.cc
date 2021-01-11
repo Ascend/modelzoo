1deb00b76eaecefc264ae1f0b5fb07857bc73228
cooem <mesiasfskvkgt@gmail.com>
2021-01-11 16:55:08 +0800

    update contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/icp_op_kernel.cc.

--- contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/icp_op_kernel.cc
+++ contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/icp_op_kernel.cc
@@ -26,6 +26,7 @@ limitations under the License.
  See the License for the specific language governing permissions and
  limitations under the License.*/
 
+
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/lum.h>
