1ff7b5e9784fafae87946f435b0f7e32f07c8f67
cooem <mesiasfskvkgt@gmail.com>
2021-01-11 16:55:25 +0800

    update contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/pcl_demo.cc.

--- contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/pcl_demo.cc
+++ contrib/Research/cv/vid2depth/vid2depth_tf_hw62279007/ops/pcl_demo.cc
@@ -26,6 +26,7 @@ limitations under the License.
  See the License for the specific language governing permissions and
  limitations under the License.*/
 
+
 #include <math.h>
 #include <stddef.h>
 #include <stdint.h>
