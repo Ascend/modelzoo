9616c15e25eaddc945c6008cda4028435188afd1
cooem <mesiasfskvkgt@gmail.com>
2021-01-11 16:46:13 +0800

    update contrib/Research/cv/east/east_tf_hw62279007/data_util.py.

--- contrib/Research/cv/east/east_tf_hw62279007/data_util.py
+++ contrib/Research/cv/east/east_tf_hw62279007/data_util.py
@@ -29,6 +29,7 @@
 this file is modified from keras implemention of data process multi-threading,
 see https://github.com/fchollet/keras/blob/master/keras/utils/data_utils.py
 '''
+
 import time
 import numpy as np
 import threading
