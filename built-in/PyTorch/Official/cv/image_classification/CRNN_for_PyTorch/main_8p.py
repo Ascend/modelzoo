4a3495f65d339436602f93bb2a188070451100c0
z00576879 <zhusiyi4@huawei.com>
2021-01-13 14:44:43 +0800

    fix protobuf bug

--- built-in/PyTorch/Official/cv/image_classification/CRNN_for_PyTorch/main_8p.py
+++ built-in/PyTorch/Official/cv/image_classification/CRNN_for_PyTorch/main_8p.py
@@ -8,7 +8,6 @@ import utils
 import torch
 import torch.nn.parallel
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 from apex import amp
 from easydict import EasyDict as edict
 
@@ -155,11 +154,10 @@ def main():
 
     converter = utils.strLabelConverter(config.DATASET.ALPHABETS)
     checkpoint_dir, log_dir = utils.create_output_folder(config)
-    writer = SummaryWriter(log_dir)
     for epoch in range(last_epoch, config.TRAIN.END_EPOCH):
         train(config, train_loader, train_dataset, converter, model, criterion, optimizer, device, epoch, npus_per_node,
-              npu, writer)
-        acc = validate(config, val_loader, val_dataset, converter, model, criterion, device, epoch, writer)
+              npu)
+        acc = validate(config, val_loader, val_dataset, converter, model, criterion, device, epoch)
         is_best = acc > best_acc
         best_acc = max(acc, best_acc)
         print("is best:", is_best)
@@ -184,11 +182,8 @@ def main():
                 }, os.path.join(checkpoint_dir, "checkpoint_{}_acc_{:.4f}.pth".format(epoch, acc))
             )
 
-    writer.close()
 
-
-def train(config, train_loader, dataset, converter, model, criterion, optimizer, device, epoch, npus_per_node, npu,
-          writer):
+def train(config, train_loader, dataset, converter, model, criterion, optimizer, device, epoch, npus_per_node, npu):
     utils.seed_everything()
     batch_time = utils.AverageMeter()
     data_time = utils.AverageMeter()
@@ -229,13 +224,12 @@ def train(config, train_loader, dataset, converter, model, criterion, optimizer,
                 epoch, i, len(train_loader), batch_time=batch_time,
                 data_time=data_time, fps=fps, loss=losses)
             print(msg)
-        # writer.add_scalar('train_loss', losses.avg, epoch * len(train_loader) + i)
         end = time.time()
     print("[npu id:", npu, "]",
           ' * FPS@all {:.3f}'.format(npus_per_node * config.TRAIN.BATCH_SIZE_PER_GPU * 1000 / batch_time.avg))
 
 
-def validate(config, val_loader, dataset, converter, model, criterion, device, epoch, writer):
+def validate(config, val_loader, dataset, converter, model, criterion, device, epoch):
     losses = utils.AverageMeter()
     model.eval()
     n_correct = 0
@@ -270,7 +264,6 @@ def validate(config, val_loader, dataset, converter, model, criterion, device, e
     print(n_total)
     accuracy = n_correct / float(n_total)
     print('Test loss: {:.4f}, accuracy: {:.4f}'.format(losses.avg, accuracy))
-    # writer.add_scalar('valid_acc', accuracy, epoch * len(val_loader) + i)
     return accuracy
 
 
