7d6e30b51bd8a0dcf6ad2ebd755f5e8145fc314c
xiaoqiqiya <zhongqi_geek@163.com>
2021-01-22 10:58:41 +0800

    update contrib/Research/cv/inceptionv3/inceptionv3_tf_xiaoqiqiya/offline_inference

---
+++ contrib/Research/cv/inceptionv3/inceptionv3_tf_xiaoqiqiya/offline_inference/resnet152_preprocess.sh
@@ -0,0 +1,1 @@
+python3 resnet152_data_preprocess.py /home/HwHiAiUser/zjut/datasets/val.tfrecord  /mnt/sdc/data/imageNet
---
+++ contrib/Research/cv/inceptionv3/inceptionv3_tf_xiaoqiqiya/offline_inference/start_inference.sh
@@ -0,0 +1,1 @@
+python3 ./msame_inference.py /home/HwHiAiUser/zjut/datasets/imageNet/val  /mnt/sdc/data/imageNet  /mnt/sdc/data/imageNet/inference /home/HwHiAiUser/zjut/pb/1202pb/resnet152/resnet152.om
