5ea059bdd4b4547c008954eddd6bf8d819f5fe96
thxloveyyj <tanghuixin@huawei.com>
2021-02-03 13:54:43 +0800

    nnmf&char-level_cnn

---
+++ contrib/TensorFlow/Research/nlp/char-level_cnn/text-classification-cnn-rnn/helper/cnews_group.py
@@ -0,0 +1,87 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+将文本整合到 train、test、val 三个文件中
+"""
+
+import os
+
+def _read_file(filename):
+    """读取一个文件并转换为一行"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return f.read().replace('\n', '').replace('\t', '').replace('\u3000', '')
+
+def _count_num(cat_dir):
+    count_num = 0
+    for fn in os.listdir(cat_dir):
+        count_num += 1
+    print('count_num:', count_num)
+    return count_num
+
+def save_file(dirname):
+    """
+    将多个文件整合并存到3个文件中
+    dirname: 原数据目录
+    文件内容格式:  类别\t内容
+    """
+    f_train = open('data/thucnews/thucnews.train.txt', 'w', encoding='utf-8')
+    f_test = open('data/thucnews/thucnews.test.txt', 'w', encoding='utf-8')
+    f_val = open('data/thucnews/thucnews.val.txt', 'w', encoding='utf-8')
+    for category in os.listdir(dirname):   # 分类目录
+        cat_dir = os.path.join(dirname, category)
+        if not os.path.isdir(cat_dir):
+            continue
+        count_num = _count_num(cat_dir)
+        files = os.listdir(cat_dir)
+        count = 0
+        for cur_file in files:
+            filename = os.path.join(cat_dir, cur_file)
+            content = _read_file(filename)
+            if count < count_num * 0.8:  # 训练集占比
+                f_train.write(category + '\t' + content + '\n')
+            elif count < count_num * 0.95:  # 测试集占比
+                f_test.write(category + '\t' + content + '\n')
+            else:  #  验证集占比
+                f_val.write(category + '\t' + content + '\n')
+            count += 1
+
+        print('Finished:', category)
+
+    f_train.close()
+    f_test.close()
+    f_val.close()
+
+if __name__ == '__main__':
+    save_file('data/thucnews')
+    print(len(open('data/thucnews/thucnews.train.txt', 'r', encoding='utf-8').readlines()))
+    print(len(open('data/thucnews/thucnews.test.txt', 'r', encoding='utf-8').readlines()))
+    print(len(open('data/thucnews/thucnews.val.txt', 'r', encoding='utf-8').readlines()))
