f4217b9fcdbba4bf63a8169b586e7bc913944d83
hongming <hithongming@163.com>
2021-02-04 21:43:52 +0800

    update convert img_2_bin

--- contrib/TensorFlow/Research/cv/dbresnet/dbresnet_tf_ihongming/train.py
+++ contrib/TensorFlow/Research/cv/dbresnet/dbresnet_tf_ihongming/train.py
@@ -118,7 +118,7 @@ def main():
     if cfg.LR == 'exponential_decay':
         learning_rate_fn = learning_rate_with_exponential_decay()
     elif cfg.LR == 'paper_decay':
-        learning_rate_fn = learning_rate_with_decay(start_lr=0.0035, power=0.9)
+        learning_rate_fn = learning_rate_with_decay(start_lr=cfg.TRAIN.LEARNING_RATE, power=0.9)
     else:
         assert 0, 'error Learning_rate'
 
@@ -148,7 +148,9 @@ def main():
         batch_norm_updates_op = tf.group(*tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope))
 
         if cfg.PLATFORM == "GPU":
-            grads = opt.compute_gradients(total_loss)
+            loss_scaling = 2 ** 12
+            grads = opt.compute_gradients(total_loss * loss_scaling)
+            grads = [(grad / loss_scaling, var) for grad, var in grads]
             apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
         elif cfg.PLATFORM == "NPU":
             # grads = opt.compute_gradients(total_loss)
@@ -156,7 +158,7 @@ def main():
             loss_scaling = 2 ** 12
             grads = opt.compute_gradients(total_loss * loss_scaling)
             grads = [(grad / loss_scaling, var) for grad, var in grads]
-            grads = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in grads if grad is not None]
+            # grads = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in grads if grad is not None]
             apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
         else:
             assert 0, 'Wrong Platform!'
