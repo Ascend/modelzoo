4e6625dc534332076cc470e902f6436d1b5da05a
Cheng-Ze Lu <lgyommg@gmail.com>
2021-02-06 20:37:14 +0800

    prepare for PR

--- contrib/Research/cv/MMNet/const.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-
-TF_SESSION_CONFIG = tf.ConfigProto(
-    gpu_options=tf.GPUOptions(allow_growth=True),
-    log_device_placement=False,
-    device_count={"GPU": 1})
-
-CKPT_EXTENSION = ".my-ckpt"
