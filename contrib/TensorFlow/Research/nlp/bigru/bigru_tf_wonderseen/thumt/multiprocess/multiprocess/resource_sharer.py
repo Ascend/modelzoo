e78d8fa2517a9431e9c35169865edc80584e0383
WonderSeen <wonderseen@outlook.com>
2021-02-08 14:42:25 +0800

    update multiprocess/multiprocess/resource_sharer.py.

--- contrib/TensorFlow/Research/nlp/bigru/bigru_tf_wonderseen/thumt/multiprocess/multiprocess/resource_sharer.py
+++ contrib/TensorFlow/Research/nlp/bigru/bigru_tf_wonderseen/thumt/multiprocess/multiprocess/resource_sharer.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2006-2008, R Oudkerk
+# Licensed to PSF under a Contributor Agreement.
 #
 # We use a background thread for sharing fds on Unix, and for sharing sockets on
 # Windows.
