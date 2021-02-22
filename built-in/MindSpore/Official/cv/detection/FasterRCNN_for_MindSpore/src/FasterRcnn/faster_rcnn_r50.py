ef388d08836e5ed728b1f88d7073d1c9ee345bdd
z00475769 <zhaochaoaww@163.com>
2021-02-22 20:25:31 +0800

    [c] MindSpore FasterRCNN模型训练调整支持修改分辨率

--- built-in/MindSpore/Official/cv/detection/FasterRCNN_for_MindSpore/src/FasterRcnn/faster_rcnn_r50.py
+++ built-in/MindSpore/Official/cv/detection/FasterRCNN_for_MindSpore/src/FasterRcnn/faster_rcnn_r50.py
@@ -85,7 +85,9 @@ class Faster_Rcnn_Resnet50(nn.Cell):
         # Fpn
         self.fpn_ncek = FeatPyramidNeck(config.fpn_in_channels,
                                         config.fpn_out_channels,
-                                        config.fpn_num_outs)
+                                        config.fpn_num_outs,
+                                        config.img_height,
+                                        config.img_width)
 
         # Rpn and rpn loss
         self.gt_labels_stage1 = Tensor(np.ones((self.train_batch_size, config.num_gts)).astype(np.uint8))
