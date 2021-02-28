bdbd5bdc59a5f740dcb626d54fa71f0c3d55f1b3
fighting <guyiyi_xue@163.com>
2021-02-28 16:16:15 +0800

    update contrib/TensorFlow/Research/cv/i3d/infer/img_to_bin.py.

--- contrib/TensorFlow/Research/cv/i3d/infer/img_to_bin.py
+++ contrib/TensorFlow/Research/cv/i3d/infer/img_to_bin.py
@@ -92,63 +92,6 @@ def main(dataset, mode, split):
             tf.float32, [None, None, _FRAME_SIZE, _FRAME_SIZE, _CHANNEL['flow']])
         info_flow, _ = flow_data.gen_test_list()
 
-#     # insert the model
-#     if mode in ['rgb', 'mixed']:
-#         with tf.variable_scope(_SCOPE['rgb']):
-#             rgb_model = i3d.InceptionI3d(
-#                 400, spatial_squeeze=True, final_endpoint='Logits')
-#             rgb_logits, _ = rgb_model(
-#                 rgb_holder, is_training=False, dropout_keep_prob=1)
-#             rgb_logits_dropout = tf.nn.dropout(rgb_logits, 1)
-#             rgb_fc_out = tf.layers.dense(
-#                 rgb_logits_dropout, _CLASS_NUM[dataset], use_bias=True)
-#             rgb_top_1_op = tf.nn.in_top_k(rgb_fc_out, label_holder, 1)
-#     if mode in ['flow', 'mixed']:
-#         with tf.variable_scope(_SCOPE['flow']):
-#             flow_model = i3d.InceptionI3d(
-#                 400, spatial_squeeze=True, final_endpoint='Logits')
-#             flow_logits, _ = flow_model(
-#                 flow_holder, is_training=False, dropout_keep_prob=1)
-#             flow_logits_dropout = tf.nn.dropout(flow_logits, 1)
-#             flow_fc_out = tf.layers.dense(
-#                 flow_logits_dropout, _CLASS_NUM[dataset], use_bias=True)
-#             flow_top_1_op = tf.nn.in_top_k(flow_fc_out, label_holder, 1)
-
-#     # construct two separate feature map and saver(rgb_saver,flow_saver)
-#     variable_map = {}
-#     if mode in ['rgb', 'mixed']:
-#         for variable in tf.global_variables():
-#             tmp = variable.name.split('/')
-#             if tmp[0] == _SCOPE['rgb']:
-#                 variable_map[variable.name.replace(':0', '')] = variable
-#         rgb_saver = tf.train.Saver(var_list=variable_map)
-#     variable_map = {}
-#     if mode in ['flow', 'mixed']:
-#         for variable in tf.global_variables():
-#             tmp = variable.name.split('/')
-#             if tmp[0] == _SCOPE['flow']:
-#                 variable_map[variable.name.replace(':0', '')] = variable
-#         flow_saver = tf.train.Saver(var_list=variable_map, reshape=True)
-
-#     # Edited Version by AlexHu
-#     if mode == 'rgb':
-#         fc_out = rgb_fc_out
-#         softmax = tf.nn.softmax(fc_out)
-#     if mode == 'flow':
-#         fc_out = flow_fc_out
-#         softmax = tf.nn.softmax(fc_out)
-#     if mode == 'mixed':
-#         fc_out = _MIX_WEIGHT_OF_RGB * rgb_fc_out + _MIX_WEIGHT_OF_FLOW * flow_fc_out
-#         softmax = tf.nn.softmax(fc_out)
-#     top_k_op = tf.nn.in_top_k(softmax, label_holder, 1)
-
-#     # start a new session and restore the fine-tuned model
-#     sess = tf.Session()
-#     sess.run(tf.global_variables_initializer())
-#     if mode in ['rgb', 'mixed']:
-#         rgb_saver.restore(sess, _CHECKPOINT_PATHS['rgb'])
-#     if mode in ['flow', 'mixed']:
-#         flow_saver.restore(sess, _CHECKPOINT_PATHS['flow'])
 
     if mode in ['rgb', 'mixed']:
         # Start Queue
@@ -201,71 +144,6 @@ def main(dataset, mode, split):
         if mode in ['rgb', 'mixed']:
             video_name = rgb_data.videos[i].name
             input_label.tofile('../data/label/' + video_name + '.bin')
-
-
-#         # Extract features from rgb and flow
-#         if mode in ['rgb']:
-#             top_1, predictions, curr_rgb_fc_data = sess.run(
-#                 [top_k_op, fc_out, rgb_fc_out],
-#                 feed_dict={rgb_holder: input_rgb,
-#                            label_holder: input_label})
-#         if mode in ['flow']:
-#             top_1, predictions, curr_flow_fc_data = sess.run(
-#                 [top_k_op, fc_out, flow_fc_out],
-#                 feed_dict={flow_holder: input_flow,
-#                            label_holder: input_label})
-#         if mode in ['mixed']:
-#             top_1, predictions, curr_rgb_fc_data, curr_flow_fc_data = sess.run(
-#                 [top_k_op, fc_out, rgb_fc_out, flow_fc_out],
-#                 feed_dict={rgb_holder: input_rgb, flow_holder: input_flow,
-#                            label_holder: input_label})
-#         if mode in ['rgb', 'mixed']:
-#             rgb_fc_data[i, :] = curr_rgb_fc_data
-#         if mode in ['flow', 'mixed']:
-#             flow_fc_data[i, :] = curr_flow_fc_data
-#         label_data[i, :] = label
-
-#         tmp = np.sum(top_1)
-#         true_count += tmp
-#         print('Video %d: %d, accuracy: %.4f (%d/%d) , name: %s' %
-#               (i+1, tmp, true_count/video_size, true_count, video_size, video_name))
-#         logging.info('Video%d: %d, accuracy: %.4f (%d/%d) , name:%s' %
-#                      (i+1, tmp, true_count/video_size, true_count, video_size, video_name))
-
-#         # self_added
-# #        print(predictions[0, np.argmax(predictions, axis=1)[0]])
-# #        print(trans_label(np.argmax(predictions, axis=1)[0], label_map))
-#         # print(np.argmax(label))
-#         #print(trans_label(np.argmax(label), label_map))
-
-#         if tmp == 0:
-#             wrong_answer = np.argmax(predictions, axis=1)[0]
-#             # Attention: the graph output are converted into the type of numpy.array
-#             print('---->answer: %s, probability: %.2f' %
-#                   (trans_label(wrong_answer, label_map), predictions[0, wrong_answer]))
-#             logging.info('---->answer: %s, probability: %.2f' %
-#                          (trans_label(wrong_answer, label_map), predictions[0, wrong_answer]))
-#             error_record.write(
-#                 'video: %s, answer: %s, probability: %.2f\n' %
-#                 (video_name, trans_label(wrong_answer, label_map),
-#                  predictions[0, wrong_answer]))
-#     error_record.close()
-#     accuracy = true_count / video_size
-#     print('test accuracy: %.4f' % (accuracy))
-#     logging.info('test accuracy: %.4f' % (accuracy))
-#     if mode in ['rgb', 'mixed']:
-#         np.save(os.path.join(log_dir, 'obj_{}_rgb_fc_{}.npy').format(
-#             dataset, accuracy), rgb_fc_data)
-#     if mode in ['flow', 'mixed']:
-#         np.save(os.path.join(log_dir, 'obj_{}_flow_fc_{}.npy').format(
-#             dataset, accuracy), flow_fc_data)
-#     np.save(os.path.join(log_dir, 'obj_{}_label.npy').format(dataset), label_data)
-
-#     if mode in ['rgb', 'mixed']:
-#         rgb_queue.close_queue()
-#     if mode in ['flow', 'mixed']:
-#         flow_queue.close_queue()
-#     sess.close()
 
 
 if __name__ == '__main__':
