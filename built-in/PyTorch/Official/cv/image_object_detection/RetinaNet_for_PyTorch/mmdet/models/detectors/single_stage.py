f22cf6ff190762f34e3c573cb558a2dfc1d4c251
敖江东 <aojiangdong@huawei.com>
2021-03-09 17:02:41 +0800

    to device in single_stage.

--- built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/models/detectors/single_stage.py
+++ built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/models/detectors/single_stage.py
@@ -89,6 +89,12 @@ class SingleStageDetector(BaseDetector):
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
+        img = img.npu()
+        for i in range(len(gt_bboxes)):
+            gt_bboxes[i] = gt_bboxes[i].npu()
+        for i in range(len(gt_labels)):
+            gt_labels[i] = gt_labels[i].npu()
+        
         super(SingleStageDetector, self).forward_train(img, img_metas)
         x = self.extract_feat(img)
         losses = self.bbox_head.forward_train(x, img_metas, gt_bboxes,
@@ -109,7 +115,8 @@ class SingleStageDetector(BaseDetector):
                 The outer list corresponds to each image. The inner list
                 corresponds to each class.
         """
-        img = img.unsqueeze(0)
+        if img.dim() == 5:
+            img = img.unsqueeze(0)
         x = self.extract_feat(img)
         outs = self.bbox_head(x)
         bbox_list = self.bbox_head.get_bboxes(
