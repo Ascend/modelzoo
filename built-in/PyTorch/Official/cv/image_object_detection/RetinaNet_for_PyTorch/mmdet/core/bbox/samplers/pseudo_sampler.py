533f3a3328f31915253ff01e00f5dbc1257d6edd
赵焱 <zhaoyan71@huawei.com>
2021-03-10 19:27:03 +0800

    add huawei copyright

--- built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/core/bbox/samplers/pseudo_sampler.py
+++ built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/core/bbox/samplers/pseudo_sampler.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 
 from ..builder import BBOX_SAMPLERS
@@ -31,10 +45,7 @@ class PseudoSampler(BaseSampler):
         Returns:
             :obj:`SamplingResult`: sampler results
         """
-        # pos_inds = torch.nonzero(
-        #     assign_result.gt_inds > 0, as_tuple=False).squeeze(-1).unique()
-        # neg_inds = torch.nonzero(
-        #     assign_result.gt_inds == 0, as_tuple=False).squeeze(-1).unique()
+
         pos_inds = assign_result.gt_inds > 0
         neg_inds = assign_result.gt_inds == 0
         gt_flags = bboxes.new_zeros(bboxes.shape[0], dtype=torch.uint8)
