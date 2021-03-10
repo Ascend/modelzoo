6f0594010cf3c32c6ba2c3f24bff2bca1c8492b8
敖江东 <aojiangdong@huawei.com>
2021-03-10 11:53:13 +0800

    fix a bug.

--- built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/models/detectors/single_stage.py
+++ built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/mmdet/models/detectors/single_stage.py
@@ -115,9 +115,9 @@ class SingleStageDetector(BaseDetector):
                 The outer list corresponds to each image. The inner list
                 corresponds to each class.
         """
-        img = img.npu()
         if img.dim() == 3:
-            img = img.unsqueeze(0).contiguous()
+            img = img.unsqueeze(0)
+        img = img.npu()
         x = self.extract_feat(img)
         outs = self.bbox_head(x)
         bbox_list = self.bbox_head.get_bboxes(
