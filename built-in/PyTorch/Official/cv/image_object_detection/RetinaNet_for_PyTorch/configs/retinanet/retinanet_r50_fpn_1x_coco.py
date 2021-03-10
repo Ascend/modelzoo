cc8a573c498f73eb39449a2656fc57031e34cb26
赵焱 <zhaoyan71@huawei.com>
2021-03-10 10:29:45 +0800

    modify retinanet config r50 1x

--- built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/configs/retinanet/retinanet_r50_fpn_1x_coco.py
+++ built-in/PyTorch/Official/cv/image_object_detection/RetinaNet_for_PyTorch/configs/retinanet/retinanet_r50_fpn_1x_coco.py
@@ -4,4 +4,15 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='NpuFusedSGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+log_config = dict( # config to register logger hook
+    interval=50, # Interval to print the log
+    hooks=[
+        dict(type='TextLoggerHook')
+    ])
+
+dist_params = dict(backend='hccl')
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8
+)
