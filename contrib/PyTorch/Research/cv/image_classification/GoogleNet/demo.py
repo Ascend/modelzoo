b5fb403c3b3f9993551bc68184524fb794cdd6cd
yx_miao <yuxia@iscas.ac.cn>
2021-03-22 13:43:28 +0800

    Modify Bug

--- contrib/PyTorch/Research/cv/image_classification/GoogleNet/demo.py
+++ contrib/PyTorch/Research/cv/image_classification/GoogleNet/demo.py
@@ -1,15 +1,72 @@
 # -*- coding: utf-8 -*-
 """demo.py
 """
+import os
 
 import torch
 import numpy as np
 from googlenet import googlenet
+import argparse
+from apex import amp
+import apex
+import torch.distributed as dist
+parser = argparse.ArgumentParser(description=' googlenet demo ')
+parser.add_argument('--device', default='npu', type=str,
+                    help='npu or gpu')
+
+parser.add_argument('--device-list', default='0,1,2,3,4,5,6,7', type=str, help='device id list')
+parser.add_argument('--dist-backend', default='hccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--addr', default='192.168.88.3', type=str,
+                    help='master addr')
+
+'''
+print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume, map_location=loc)
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            if args.amp:
+                amp.load_state_dict(checkpoint['amp'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+
+'''
+def device_id_to_process_device_map(device_list):
+    devices = device_list.split(",")
+    devices = [int(x) for x in devices]
+    devices.sort()
+
+    process_device_map = dict()
+    for process_id, device_id in enumerate(devices):
+        process_device_map[process_id] = device_id
+
+    return process_device_map
 
 
 def build_model():
+    global loc
     # 请自定义模型并加载预训练模型
-    model = googlenet()
+    args = parser.parse_args()
+    args.process_device_map = device_id_to_process_device_map(args.device_list)
+    os.environ['MASTER_ADDR'] = args.addr
+    os.environ['MASTER_PORT'] = '29688'
+    ngpus_per_node = len(args.process_device_map)
+    
+    dist.init_process_group(backend=args.dist_backend,  # init_method=args.dist_url,
+                                    world_size=1, rank=0)
+    
+    args.gpu = args.process_device_map[0]
+    loc = 'npu:{}'.format(args.gpu)
+    torch.npu.set_device(loc)
+    
+    model = googlenet().to(loc)
+    optimizer = apex.optimizers.NpuFusedSGD(model.parameters(), 0.5,
+                                momentum=0.9,
+                                weight_decay=1.0e-04)
+    model, optimizer = amp.initialize(model, optimizer, opt_level="O2", loss_scale=1024)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], broadcast_buffers=False)
     checkpoint = torch.load('./checkpoint.pth.tar')
     model.load_state_dict(checkpoint['state_dict'])
     model.eval()  # 注意设置eval模式
@@ -59,7 +116,7 @@ if __name__ == '__main__':
     input_tensor = pre_process(raw_data)
 
     # 4. 执行forward
-    output_tensor = model(input_tensor)
+    output_tensor = model(input_tensor.to(loc))
 
     # 5. 后处理
     result = post_process(output_tensor)
