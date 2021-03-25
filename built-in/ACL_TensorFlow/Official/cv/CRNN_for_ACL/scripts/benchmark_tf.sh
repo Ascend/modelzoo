c498ccebea6eaeada0f5485ae0b4facdd25c9a4b
chengchunlei <alex.cheng@huawei.com>
2021-03-25 21:07:25 +0800

    update built-in/ACL_TensorFlow/Official/cv/CRNN_for_ACL/scripts/benchmark_tf.sh.

--- built-in/ACL_TensorFlow/Official/cv/CRNN_for_ACL/scripts/benchmark_tf.sh
+++ built-in/ACL_TensorFlow/Official/cv/CRNN_for_ACL/scripts/benchmark_tf.sh
@@ -5,7 +5,7 @@ benchmark_dir=$cur_dir/../Benchmark/out
 om_name=$cur_dir/../shadownet_tf_64batch.om
 
 #start offline inference
-$benchmark_dir/benchmark --model $om_name --input $cur_dir/img_bin/ --output output_fp32
+$benchmark_dir/benchmark --model $om_name --input $cur_dir/img_bin/ --output $cur_dir/output_fp32
 
 #post process
 python3 tools/post_precess.py
