2d9430b36e3e25ce55d3d483d1ac5da3e56adc74
啊小冬絮 <754019499@qq.com>
2021-03-27 13:07:46 +0800

    update contrib/TensorFlow/Research/cv/dpn/dpn_tf_hw34064571/train_1p.sh.

--- contrib/TensorFlow/Research/cv/dpn/dpn_tf_hw34064571/train_1p.sh
+++ contrib/TensorFlow/Research/cv/dpn/dpn_tf_hw34064571/train_1p.sh
@@ -1,2 +1,2 @@
-python3    train_npu.py  --model_path ./pre_model/dpn.ckpt-55  --data_path  /diska/xiaoqiqi/train.tfrecords --output_path  ./model_save   --epoch  57
+python3    train_npu.py  --model_path ./pre_model/dpn.ckpt  --data_path  ./data/train.tfrecords --output_path  ./model_save   --epoch  57
     
