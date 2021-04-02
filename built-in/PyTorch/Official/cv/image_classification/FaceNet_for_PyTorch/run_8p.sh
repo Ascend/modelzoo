21629f88b0ac4a10e807146cbc5e75b7b2dcdf23
zhayongliang <zhayongliang@huawei.com>
2021-04-02 09:49:29 +0800

    add facenet lreaning rate

--- built-in/PyTorch/Official/cv/image_classification/FaceNet_for_PyTorch/run_8p.sh
+++ built-in/PyTorch/Official/cv/image_classification/FaceNet_for_PyTorch/run_8p.sh
@@ -16,11 +16,11 @@ python3.7 fine_tune_new_8p.py \
     --amp_cfg \
     --opt_level O2 \
     --loss_scale_value 1024 \
-    --device_list '0,1,2,3,4,5,6,7' \
+    --device_list '0,1,2,3,4,5,6,7,8' \
     --batch_size 4096 \
     --epochs 8 \
     --epochs_per_save 1 \
-    --lr 0.008 \
+    --lr 0.005 \
     --workers 64 \
     --data_dir '/home/VGG-Face2/data/train_cropped' \
     --addr=$(hostname -I |awk '{print $1}') \
