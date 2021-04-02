7ddf1970a1fbc97a95bec0a60d36f268c589d26d
zhayongliang <zhayongliang@huawei.com>
2021-04-02 09:53:22 +0800

    add facenet learning rate

--- built-in/PyTorch/Official/cv/image_classification/FaceNet_for_PyTorch/run_8p.sh
+++ built-in/PyTorch/Official/cv/image_classification/FaceNet_for_PyTorch/run_8p.sh
@@ -16,7 +16,7 @@ python3.7 fine_tune_new_8p.py \
     --amp_cfg \
     --opt_level O2 \
     --loss_scale_value 1024 \
-    --device_list '0,1,2,3,4,5,6,7,8' \
+    --device_list '0,1,2,3,4,5,6,7' \
     --batch_size 4096 \
     --epochs 8 \
     --epochs_per_save 1 \
