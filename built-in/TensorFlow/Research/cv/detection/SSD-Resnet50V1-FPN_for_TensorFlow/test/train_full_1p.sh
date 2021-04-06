7c90e8749b0ee48811aa699c611f0d89fc9537be
linda.liuping <121377674@qq.com>
2021-04-06 10:50:07 +0800

    update SSD-Resnet50V1-FPN_for_TensorFlow/test/train_full_1p.sh.

--- built-in/TensorFlow/Research/cv/detection/SSD-Resnet50V1-FPN_for_TensorFlow/test/train_full_1p.sh
+++ built-in/TensorFlow/Research/cv/detection/SSD-Resnet50V1-FPN_for_TensorFlow/test/train_full_1p.sh
@@ -8,7 +8,7 @@ RANK_ID_START=0
 #参数配置
 #base param, using user config in python scripts if not config in this shell
 num_train_steps=0
-ckpt_path=$cur_path/checkpoints
+ckpt_path=/checkpoints
 pipeline_config=$cur_path/models/research/configs/ssd320_full_1gpus.config
 
 #npu param
@@ -48,11 +48,21 @@ fi
 
 #############执行训练#########################
 cd $cur_path/models/research
+if [  -f ${pipeline_config}.bak];then
+   cp ${pipeline_config}.bak ${pipeline_config}
+else
+   cp ${pipeline_config} ${pipeline_config}.bak
+fi
+
+sed -i "s%/checkpoints%${ckpt_path}%p" ${pipeline_config} 
+sed -i "s%/data/coco2017_tfrecords%${data_path}/coco2017_tfrecords%p" ${pipeline_config} 
+
 
 start=$(date +%s)
 
 for((RANK_ID=$RANK_ID_START;RANK_ID<$((RANK_SIZE+RANK_ID_START));RANK_ID++));
  do
+  export RANK_ID=$RANK_ID
   if [-d $cur_path/test/output ];then
      rm -rf $cur_path/test/output/*
      mkdir -p $cur_path/test/output/$ASCEND_DEVICE_ID
